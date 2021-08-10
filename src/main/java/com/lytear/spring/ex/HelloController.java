36b29aadfb942a963fe8b2b6e3486f6c9f3a481c
LAPTOP-UMHV38EN\윤진일 <ytonyyun@naver.com>
2021-08-10 15:25:40 +0900

    [윤진일] - 최초 파일 보기

---
+++ src/main/java/com/lytear/spring/ex/HelloController.java
@@ -0,0 +1,27 @@
+package com.lytear.spring.ex;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloworld() {
+		return "Hello World";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
